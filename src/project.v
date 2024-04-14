6d8db0fb37790e9e01346a830802fb6d547a720b
Ziyad Edher <ziyad.edher@gmail.com>
2024-04-14 14:42:25 -0700

    try 1

--- src/project.v
+++ src/project.v
@@ -5,6 +5,7 @@
 
 `define default_netname none
 
+// input-path IO 0: programming mode
 module tt_um_ziyadedher_trash (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
@@ -19,84 +20,120 @@ module tt_um_ziyadedher_trash (
     assign uio_out = 0;
     assign uio_oe  = 0;
 
-    // TODO: Now that we can have two 8-bit inputs and a separate clock, we can simplify this module a lot.
+    wire reset = ! rst_n;
 
-    wire clk2 = ui_in[0];
-    wire [2:0] ctrl = ui_in[3:1];
-    wire [3:0] data = ui_in[7:4];
-    // wire [6:0] led_out;
-    // assign io_out[6:0] = led_out;
-    // wire [5:0] seed_input = io_in[7:2];
+    // 8 bytes of program memory.
+    // This can be addressed by 3 bits, which we have a program counter for.
+    reg [7:0] program [7:0];
+    reg [2:0] pc = 0;
 
-    reg [8:0] operand1;
-    reg [8:0] operand2;
-    // For now we're commenting this out and leaving the results unbuffered.
-    // reg [8:0] result_out;
-    // assign io_out = result_out;
+    // 16-byte memory hell yeah.
+    // This can be addressed by 4 bits!
+    reg [7:0] memory [15:0];
 
-    always @(posedge clk2) begin
-        if (!ctrl[0]) begin  // if first CTRL bit is off, we're in STORE mode
-            if (!ctrl[1]) begin  // second CTRL bit controls whether it's the first or second operand
-                if (!ctrl[2]) begin  // third CTRL bit controls whether it's the upper or lower half
-                    operand1[3:0] <= data;
-                end else begin
-                    operand1[7:4] <= data;
-                end
-            end else begin
-                if (!ctrl[2]) begin
-                    operand2[3:0] <= data;
-                end else begin
-                    operand2[7:4] <= data;
-                end
-            end
-        end else begin  // if first CTRL bit is on, this is reserved.
-            // TODO
-            // if (!ctrl[1] && !ctrl[2]) begin
-            //     result_out[7:0] <= 0;
-            // end
+    // 4 8-bit registers.
+    // We can reference a specfic register using 2 bits.
+    reg [7:0] r0, r1, r2, r3;
+
+    assign in = {ui_in, uio_in};
+
+    // Programming mode.
+    // When this is set to 0, the device is in programming mode.
+    // When this is set to 1, the device is in execution mode.
+    wire prog = in[0];
+    
+    // PROGRAMMING MODE
+    // | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | A | B | C | D | E | F |
+    // | 0 |                    DATA TO PROGRAM                        |
+    //
+    // Every clock cycle, the device will read the 15 bits of data from the input path,
+    // and write it to the program memory at the address specified by the program counter.
+    // The program counter will increment by 1 every clock cycle.
+    always @(posedge clk) begin
+        if (reset) begin
+            pc <= 0;
+        end else if (! prog) begin
+            program[pc] <= in[15:1];
+            pc <= pc + 1;
         end
     end
 
-    // Compute result_out in terms of operand1, operand2
-    fp8mul mul1(
-        .sign1(operand1[7]),
-        .exp1(operand1[6:3]),
-        .mant1(operand1[2:0]),
-        .sign2(operand2[7]),
-        .exp2(operand2[6:3]),
-        .mant2(operand2[2:0]),
-        .sign_out(uo_out[7]),
-        .exp_out(uo_out[6:3]),
-        .mant_out(uo_out[2:0])
-    );
+    // EXECUTION MODE
+    // | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | A | B | C | D | E | F |
+    // | 1 | OPERATION |           OPERATION-SPECIFIC DATA             |
+    //
+    // * NOOP       (0x0)   no operation
+    // | 1 |    0x0    |                   <unused>                    |
+    // * STORE      (0x1)   stores DATA (1 byte) in REG
+    // | 1 |    0x1    |      REG      |              DATA             |
+    // * CALC       (0x2)   executes REG_OUT = ALU(OPCODE, REG_IN[7:4], REG_IN[3:0])
+    // | 1 |    0x2    |     OPCODE    |     REG_IN    |    REG_OUT    |
+    // * MEMSTORE   (0x3)   stores DATA (1 byte) at MEM[ADDR]
+    // | 1 |    0x3    |      ADDR     |              DATA             |
+    // * MEMLOAD    (0x4)   loads 1 byte of data from MEM[ADDR] to REG
+    // | 1 |    0x4    |      ADDR     |      REG      |    <unused>   |
+    // * JUMP       (0x5)   jumps to ADDR
+    // | 1 |    0x5    |      ADDR     |              <unused>         |
+    // * JUMPIF     (0x6)   jumps to ADDR if data at REG_A == data at REG_B
+    // | 1 |    0x6    |      ADDR     |     REG_A     |     REG_B     |
+    // * OUT        (0x7)   outputs REG to dedicated output
+    // | 1 |    0x7    |      REG      |              <unused>         |
+    always @(posedge clk) begin
+        if (reset) begin
+            pc <= 0;
+        end else if (prog) begin
+            case (in[3:1])
+                4'b000 : begin 
+                end;
+                4'b001 : begin
+                    case (in[7:4])
+                        2'b0000 : r0 <= in[15:8];
+                        2'b0001 : r1 <= in[15:8];
+                        2'b0010 : r2 <= in[15:8];
+                        2'b0011 : r3 <= in[15:8];
+                    endcase
+                end;
+                4'b010 : begin
+                    case (in[15:12])
+                        2'b0000 : alu(.clk(clk), .opcode(in[7:4]), .a(r0[7:4]), .b(r0[3:0]), .res(r1[7:0]));
+                    endcase
+                end;
+                // TODO: the rest
+                default;
+            endcase
+            pc <= pc + 1;
+        end
+    end
 endmodule
 
-module fp8mul (
-  input sign1,
-  input [3:0] exp1,
-  input [2:0] mant1,
 
-  input sign2,
-  input [3:0] exp2,
-  input [2:0] mant2,
-
-  output sign_out,
-  output [3:0] exp_out,
-  output [2:0] mant_out
+// 4-bit ALU
+module alu (
+    input clk,
+    input [3:0] opcode,
+    input [3:0] a,
+    input [3:0] b,
+    output [7:0] res,
 );
-    parameter EXP_BIAS = 7;
-    wire isnan = (sign1 == 1 && exp1 == 0 && mant1 == 0) || (sign2 == 1 && exp2 == 0 && mant2 == 0);
-    wire [7:0] full_mant = ({exp1 != 0, mant1} * {exp2 != 0, mant2});
-    wire overflow_mant = full_mant[7];
-    wire [6:0] shifted_mant = overflow_mant ? full_mant[6:0] : {full_mant[5:0], 1'b0};
-    // is the mantissa overflowing up to the next exponent?
-    wire roundup = (exp1 + exp2 + overflow_mant < 1 + EXP_BIAS) && (shifted_mant[6:0] != 0)
-                   || (shifted_mant[6:4] == 3'b111 && shifted_mant[3]);
-    wire underflow = (exp1 + exp2 + overflow_mant) < 1 - roundup + EXP_BIAS;
-    wire is_zero = exp1 == 0 || exp2 == 0 || isnan || underflow;
-    // note: you can't use negative numbers reliably. just keep things positive during compares.
-    wire [4:0] exp_out_tmp = (exp1 + exp2 + overflow_mant + roundup) < EXP_BIAS ? 0 : (exp1 + exp2 + overflow_mant + roundup - EXP_BIAS);
-    assign exp_out = exp_out_tmp > 15 ? 4'b1111 : (is_zero) ? 0 : exp_out_tmp[3:0];  // Exponent bias is 7
-    assign mant_out = exp_out_tmp > 15 ? 3'b111 : (is_zero || roundup) ? 0 : (shifted_mant[6:4] + (shifted_mant[3:0] > 8 || (shifted_mant[3:0] == 8 && shifted_mant[4])));
-    assign sign_out = ((sign1 ^ sign2) && !(is_zero)) || isnan;
+    always @(posedge clk) begin
+        case (opcode)
+            4'b0000 : op = a + b;
+            4'b0001 : op = a - b;
+            4'b0010 : op = a * b;
+            4'b0011 : op = a / b;
+            4'b0100 : op = a % b;
+            4'b0101 : op = a & b;
+            4'b0110 : op = a | b;
+            4'b0111 : op = a && b;
+            4'b1000 : op = a || b;
+            4'b1001 : op = a ^ b;
+            4'b1010 : op = ~ a;  
+            4'b1011 : op = ! a;  
+            4'b1100 : op = a >> 1;
+            4'b1101 : op = a << 1;
+            4'b1110 : op = a + 1;
+            4'b1111 : op = a - 1;
+            default : op = 8'bXXXXXXXX;
+        endcase
+    end
 endmodule
